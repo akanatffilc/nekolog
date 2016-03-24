607fd2c50308de67b36197041473cb87c6fbb55c
佐藤 弘教 <hsato@satou-no-MacBook-Air.local>
2016-03-24 15:04:56 +0900

    mainのmoduleを分離させ、ng-controllerを追記

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
-angular.module('nekolog' , []).controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+var app = angular.module('nekolog' , []);
+
+//Projects
+app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
     var $uri ='/api/v2/projects';
 
@@ -18,4 +21,21 @@ angular.module('nekolog' , []).controller('MainCtrl', ['$scope', '$http', functi
         });
     };
 
+}]);
+
+//Issue
+app.controller('IssueCtrl', ['$scope', '$http', function ($scope, $http) {
+    var $uri ='/api/v2/issues';
+    $scope.doSearch = function() {
+        $http({
+            method : 'GET',
+            url : $uri
+        }).success(function(data, status, headers, config) {
+            $scope.results = data.data;
+            console.log(status);
+            console.log(data);
+        }).error(function(data, status, headers, config) {
+            console.log(status);
+        });
+    };
 }]);
