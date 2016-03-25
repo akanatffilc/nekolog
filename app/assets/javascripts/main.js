f450e3763babafc62690e6ba1cfd3307a7c134df
佐藤 弘教 <hsato@satou-no-MacBook-Air.local>
2016-03-25 12:10:13 +0900

    mainJS add API & sign up display modify

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -39,3 +39,36 @@ app.controller('IssueCtrl', ['$scope', '$http', function ($scope, $http) {
         });
     };
 }]);
+
+//IssueTypes
+app.controller('IssueTypesGetCtrl', ['$scope', '$http', function ($scope, $http) {
+    var $uri ='/issue_types';
+    $scope.doSearch = function() {
+        $http({
+            method : 'GET',
+            url : $uri
+        }).success(function(data, status, headers, config) {
+            $scope.results = data.data;
+            console.log(status);
+            console.log(data);
+        }).error(function(data, status, headers, config) {
+            console.log(status);
+        });
+    };
+}]);
+
+app.controller('IssueTypesPostCtrl', ['$scope', '$http', function ($scope, $http) {
+    var $uri ='/issue_types';
+    $scope.doPost = function() {
+        $http({
+            method : 'POST',
+            url : $uri
+        }).success(function(data, status, headers, config) {
+            $scope.results = data.data;
+            console.log(status);
+            console.log(data);
+        }).error(function(data, status, headers, config) {
+            console.log(status);
+        });
+    };
+}]);
